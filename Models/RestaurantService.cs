82cc8a4eca2b9c9563628a2fa3c8ecf4904c515a
海蜇零 <30751915+JellyfishZero@users.noreply.github.com>
2026-06-24 19:08:02 +0800

    feat RestaurantService.cs更名。

---
+++ Models/RestaurantService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatToEat.Data;
+
+namespace WhatToEat.Models
+{
+    class RestaurantService
+    {
+        public bool NameExists(string name)
+        {
+            using var db = new AppDbContext();
+            return db.Restaurants.Any(r => r.Name == name);
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            using var db = new AppDbContext();
+            db.Restaurants.Add(restaurant);
+            db.SaveChanges();
+        }
+    }
+}
