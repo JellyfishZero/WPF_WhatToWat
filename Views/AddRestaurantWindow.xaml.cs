f3e7c3852a8b14a2979af0db3eb09c20cfff404b
海蜇零 <30751915+JellyfishZero@users.noreply.github.com>
2026-06-24 18:57:07 +0800

    feat 調整UX避免過度重複設置。

--- Views/AddRestaurantWindow.xaml.cs
+++ Views/AddRestaurantWindow.xaml.cs
@@ -71,8 +71,138 @@ namespace WhatToEat
             RestaurantNameTextBox.Clear();
             PreferenceScoreSlider.Value = 3;
             HasBusinessHoursCheckBox.IsChecked = false;
+            DefaultStartHourComboBox.SelectedIndex = 9;
+            DefaultStartMinuteComboBox.SelectedIndex = 0;
+            DefaultEndHourComboBox.SelectedIndex = 21;
+            DefaultEndMinuteComboBox.SelectedIndex = 0;
 
             ClearBusinessHours();
+        }
+
+        private void OnApplyWeekdaysBusinessHoursClicked(object sender, RoutedEventArgs e)
+        {
+            ApplyDefaultBusinessHours(includeWeekend: false);
+        }
+
+        private void OnApplyAllBusinessHoursClicked(object sender, RoutedEventArgs e)
+        {
+            ApplyDefaultBusinessHours(includeWeekend: true);
+        }
+
+        private void ApplyDefaultBusinessHours(bool includeWeekend)
+        {
+            string startHour = (string)DefaultStartHourComboBox.SelectedItem;
+            string startMinute = (string)DefaultStartMinuteComboBox.SelectedItem;
+            string endHour = (string)DefaultEndHourComboBox.SelectedItem;
+            string endMinute = (string)DefaultEndMinuteComboBox.SelectedItem;
+
+            SetBusinessHour(
+                MondayOpenCheckBox,
+                MondayStartHourComboBox,
+                MondayStartMinuteComboBox,
+                MondayEndHourComboBox,
+                MondayEndMinuteComboBox,
+                startHour,
+                startMinute,
+                endHour,
+                endMinute
+            );
+
+            SetBusinessHour(
+                TuesdayOpenCheckBox,
+                TuesdayStartHourComboBox,
+                TuesdayStartMinuteComboBox,
+                TuesdayEndHourComboBox,
+                TuesdayEndMinuteComboBox,
+                startHour,
+                startMinute,
+                endHour,
+                endMinute
+            );
+
+            SetBusinessHour(
+                WednesdayOpenCheckBox,
+                WednesdayStartHourComboBox,
+                WednesdayStartMinuteComboBox,
+                WednesdayEndHourComboBox,
+                WednesdayEndMinuteComboBox,
+                startHour,
+                startMinute,
+                endHour,
+                endMinute
+            );
+
+            SetBusinessHour(
+                ThursdayOpenCheckBox,
+                ThursdayStartHourComboBox,
+                ThursdayStartMinuteComboBox,
+                ThursdayEndHourComboBox,
+                ThursdayEndMinuteComboBox,
+                startHour,
+                startMinute,
+                endHour,
+                endMinute
+            );
+
+            SetBusinessHour(
+                FridayOpenCheckBox,
+                FridayStartHourComboBox,
+                FridayStartMinuteComboBox,
+                FridayEndHourComboBox,
+                FridayEndMinuteComboBox,
+                startHour,
+                startMinute,
+                endHour,
+                endMinute
+            );
+
+            if (!includeWeekend)
+            {
+                return;
+            }
+
+            SetBusinessHour(
+                SaturdayOpenCheckBox,
+                SaturdayStartHourComboBox,
+                SaturdayStartMinuteComboBox,
+                SaturdayEndHourComboBox,
+                SaturdayEndMinuteComboBox,
+                startHour,
+                startMinute,
+                endHour,
+                endMinute
+            );
+
+            SetBusinessHour(
+                SundayOpenCheckBox,
+                SundayStartHourComboBox,
+                SundayStartMinuteComboBox,
+                SundayEndHourComboBox,
+                SundayEndMinuteComboBox,
+                startHour,
+                startMinute,
+                endHour,
+                endMinute
+            );
+        }
+
+        private static void SetBusinessHour(
+            CheckBox openCheckBox,
+            ComboBox startHourComboBox,
+            ComboBox startMinuteComboBox,
+            ComboBox endHourComboBox,
+            ComboBox endMinuteComboBox,
+            string startHour,
+            string startMinute,
+            string endHour,
+            string endMinute
+        )
+        {
+            openCheckBox.IsChecked = true;
+            startHourComboBox.SelectedItem = startHour;
+            startMinuteComboBox.SelectedItem = startMinute;
+            endHourComboBox.SelectedItem = endHour;
+            endMinuteComboBox.SelectedItem = endMinute;
         }
 
         private Data.BusinessHour CreateBusinessHour(
