2256f2732860ab0faa01f5732b609a7b51d8763c
海蜇零 <30751915+JellyfishZero@users.noreply.github.com>
2026-06-25 16:48:38 +0800

    feat 卷軸手感優化。

--- Views/QueryRestaurantWindow.xaml.cs
+++ Views/QueryRestaurantWindow.xaml.cs
@@ -28,5 +28,53 @@ namespace WhatToEat
             // 載入資料
             _queryRestaurantVM.LoadRestaurants();
         }
+
+        #region 營業時間 DataGrid 捲動優化
+        /// <summary>
+        /// 使營業時間子表不會消耗父表的滑鼠滾輪事件，避免滑鼠滾輪在子表上時，父表不會跟著滾動。
+        /// </summary>
+        private void OnBusinessHoursDataGridPreviewMouseWheel(
+            object sender,
+            MouseWheelEventArgs e
+        )
+        {
+            e.Handled = true;
+
+            // 找到父層的 ScrollViewer，並將滾動事件傳遞給它
+            var scrollViewer = FindVisualChild<ScrollViewer>(RestaurantDataGrid);
+            if (scrollViewer is null)
+            {
+                return;
+            }
+
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta);
+        }
+
+        /// <summary>
+        /// 這個方法是用來在視覺樹中尋找指定類型的子元素。
+        /// 它會遞迴地遍歷所有子元素，直到找到第一個符合條件的子元素為止。
+        /// </summary>
+        private static T? FindVisualChild<T>(DependencyObject parent)
+            where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child is T target)
+                {
+                    return target;
+                }
+
+                var descendant = FindVisualChild<T>(child);
+                if (descendant is not null)
+                {
+                    return descendant;
+                }
+            }
+
+            return null;
+        }
+        #endregion
     }
 }
